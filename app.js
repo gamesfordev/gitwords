a2d048d9db8cc1b4e457694bffe565b75850d212
Shalitha Suranga <shalithasuranga@gmail.com>
2017-11-11 22:08:06 +0530

    Change port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();  
 const server = require('http').createServer(app);  
 const io = require('socket.io')(server);
-const port = 8080;
+const port = process.env.PORT || 8080;
 const mongo=require('./services/mongoservice').mongo
 
 app.use(express.static(__dirname));
