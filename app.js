2f923d8255d222815c65c03bdc9532d41d65608d
Shalitha Suranga <shalithasuranga@gmail.com>
2017-11-11 18:45:45 +0530

    Add files

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');  
+const app = express();  
+const server = require('http').createServer(app);  
+const io = require('socket.io')(server);
+const port = 8080;
+
+app.use(express.static(__dirname));
+
+app.get('/', function(req, res,next) {  
+    res.sendFile(__dirname + '/ui/index.html');
+});
+
+app.get('/scripts/main.js', function(req, res,next) {  
+    res.sendFile(__dirname + '/scripts/main.js');
+});
+
+app.get('/css/style.css', function(req, res,next) {  
+    res.sendFile(__dirname + '/css/style.css');
+});
+
+server.listen(port, () => {
+	console.log('app is listening to ' + port);
+});  
+
+
+io.on('connection', function(client) {  
+    console.log('Player loaded the game!');
+});
---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+        var socket = io.connect('http://localhost:8080');
+        socket.on('connect', function(data) {
+            
+        });
