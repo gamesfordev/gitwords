294272123d8bcdfdf2b7fbc0b2ba0f8b899f8271
Shalitha Suranga <shalithasuranga@gmail.com>
2017-11-11 21:03:56 +0530

    Adding functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,7 @@
 let socket = io.connect('http://localhost:8080');
+let wordsTimer = null;
+let currentWord = '';
+let loadedWord = '';
 let words = [
     {
         "splited" : ["mple", "exa"],
@@ -12,11 +15,50 @@ let words = [
 
 console.log(words);
 
+$('#commandInput').keypress(function(e) {
+    if(e.which == 13) {
+        processCommand($(this).val());
+        $(this).val('');
+    }
+});
+
+
+let processCommand = (text) => {
+    console.log(text);
+    let tokens = text.split(' ');
+    if(tokens.length > 0){
+        if(tokens[0] == 'git') {
+            if(tokens[1] == 'add') {
+                currentWord += loadedWord.splited[tokens[2]];
+            }
+            else if(tokens[1] == 'commit') {
+                if(loadedWord.correct == currentWord) {
+                    console.log('correct!!');
+                }
+                else{
+                    console.log('wrong!!');
+                }
+                currentWord = '';
+                showWord();
+            }
+        }
+    }
+};
+
+let showWord = () => {
+    let randomWord = words[parseInt(Math.random() * 1000) % words.length];
+    loadedWord = randomWord;
+    console.log(randomWord);
+};
 
 let startGame = () => {
     $('#startGame').hide();
     $('#gameScreen').show();
     socket.emit('playerConnect', '');
+    /*wordsTimer = window.setInterval(() => {
+        showWord();
+    }, 5000);*/
+    showWord();
 };
 
 socket.on('connect', function(data) {
