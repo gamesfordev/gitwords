bcd6fde57f11975676167f76ef142754f0d3e191
Shalitha Suranga <shalithasuranga@gmail.com>
2017-11-13 00:00:52 +0530

    Change display Time style

--- scripts/main.js
+++ scripts/main.js
@@ -89,8 +89,12 @@ Array.prototype.shuffle = function () {
     return this;
 }
 
+let secToDisplay = () => {
+    return '<b>' + Math.floor(myTime / 60) + '</b>min <b>' + (myTime - Math.floor(myTime/60) * 60) + '</b>sec';
+};
+
 let processCommand = (text) => {
-    console.log(text);
+    //console.log(text);
     commandHistory.push(text);
     prevCommand = -1;
     console.log(commandHistory);
@@ -175,9 +179,9 @@ let showWord = () => {
         points: chunks.length,
     };
 
-    console.log(loadedWord);
+    //console.log(loadedWord);
     //$('#pool').html(loadedWord.splited.toString());
-    console.log(chunks);
+    //console.log(chunks);
     addToPool(chunks);
 };
 
@@ -192,7 +196,7 @@ let startGame = () => {
         $('#startGame').hide();
         $('#gameScreen').show();
         $('#player').html(playerName);
-        $('#time').html(myTime);
+        $('#time').html(secToDisplay(myTime));
         $('#score').html(myScore);
         Konsole.clear();
         $('#pool').html('');
@@ -237,7 +241,7 @@ let gameTick = () => {
         gameOver();
         return;
     }
-    $('#time').html(myTime);
+    $('#time').html(secToDisplay(myTime));
 };
 
 let gameOver = () => {
@@ -264,12 +268,12 @@ let animationTick = () => {
         if($(element).data('mov') == 'yes') {
             let pos = parseFloat(document.getElementById('ck_' + i).style.top);
             let dir = parseFloat($(element).data('dir'));
-            console.log('dir',dir);
+            //console.log('dir',dir);
             pos += dir;
             if(pos < 10) $(element).data('dir', Math.abs(dir));
             if(pos > 80) $(element).data('dir', -dir);
 
-            console.log(pos);
+            //console.log(pos);
             $(element).css('top', pos + '%' );
         }
     });
@@ -291,7 +295,7 @@ socket.on('connect', function (data) {
 
 socket.on('scoreUpdate', function (res) {
     console.log("score updated")
-    console.log(res) //update leaderboard using this data
+    //console.log(res) //update leaderboard using this data
     let result = ''
 
     let sorted = res.data.sort((a, b) => {
